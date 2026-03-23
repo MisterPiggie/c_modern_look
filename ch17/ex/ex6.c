da556b396c9bd96944831e352c9fd3ddb515d323
piggie <matvey00014@gmail.com>
2026-03-24 00:48:33 +0500

    ch17 small addition

--- ch17/ex/ex6.c
+++ ch17/ex/ex6.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
 void *delete_from_list(struct node **list, int n)
 {
-    struct node *item = *list;
-    while (item)
+    struct node **cursor = list;
+    while (*cursor != NULL)
     {
-        if (item -> value == n)
+        if ((*item) -> value == n)
         {
-            *list = item -> next;
-            free(item);
+            struct node to_free = *cursor;
+            *cursor = (*cursor) -> next;
+            free(to_free);
             break;
         }
-        list = &item -> next;
-        item = item -> next;
+        cursor = &(*cursor) -> next;
+
     }
 }
