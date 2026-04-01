7dfd0ba6f9da017063b55f5f67fa412e14eb0b35
piggie <matvey00014@gmail.com>
2026-04-01 23:34:53 +0500

    ch20 had a rough two days, coming back to working

--- ch20/ex5.c
+++ ch20/ex5.c
@@ -1,2 +1,4 @@
-#define MK_COLOR(r,g,b) ((long) (b) << 16, (g) << 8, (r))
+#define GET_RED(color) ((u8) ((color) & 0xFF))
+#define GET_GREEN(color) ((u8) ((color >> 8) & 0xFF))
+#define GET_BLUE(color) ((u8) ((color >> 16) & 0xFF))
 
--- ch20/ex6.c
+++ ch20/ex6.c
@@ -7,7 +7,15 @@ int main(void)
     unsigned short num;
     printf("Enter a hexadecimal number (up to for digits): ");
     scanf("%hx", &num);
-// 1111 
-    printf("Number with bytes swapped: %hx\n", num);
+    printf("Number with bytes swapped: %hx\n", swap_bytes(num));
     return 0;
+}
+
+
+unsigned short swap_bytes(unsigned short i)
+{
+    unsigned short low_bits = i >> 8;
+    unsigned short high_bits = i << 8;
+    // return i >> 8 | i << 8;
+    return high_bits | low_bits;
 }
